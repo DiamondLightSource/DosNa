932b5c8511d38c3a42bb9eb7b60b6e8604ed0231
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2017-11-06 09:12:39 +0000

    Rename to test_connection and use parameter --connection

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,8 +1,8 @@
 
 
+import logging as logging
 import sys
 import unittest
-import logging as logging
 
 import dosna as dn
 
@@ -11,7 +11,7 @@ log = logging.getLogger()
 log.level = logging.INFO
 
 
-class ClusterTest(unittest.TestCase):
+class ConnectionTest(unittest.TestCase):
 
     BACKEND = 'ram'
     ENGINE = 'cpu'
@@ -24,7 +24,7 @@ class ClusterTest(unittest.TestCase):
                  .format(self.BACKEND, self.ENGINE, self.CLUSTER_CONFIG))
 
         dn.use(backend=self.BACKEND, engine=self.ENGINE)
-        self.C = dn.Cluster(**self.CLUSTER_CONFIG)
+        self.C = dn.Connection(**self.CLUSTER_CONFIG)
 
     def tearDown(self):
         log.removeHandler(self.handler)
@@ -35,7 +35,7 @@ class ClusterTest(unittest.TestCase):
         self.assertIn(self.ENGINE, dn.engines.available)
 
     def test_connection(self):
-        c = dn.Cluster(**self.CLUSTER_CONFIG)
+        c = dn.Connection(**self.CLUSTER_CONFIG)
         self.assertIsNotNone(c)
         self.assertFalse(c.connected)
         c.connect()
@@ -51,8 +51,9 @@ if __name__ == "__main__":
                         help='Select backend (ram | hdf5 | ceph)')
     parser.add_argument('--engine', dest='engine', default='cpu',
                         help='Select engine (cpu | joblib | mpi)')
-    parser.add_argument('--cluster', dest='cluster', default='test-cluster',
-                        help='Cluster name')
+    parser.add_argument('--connection', dest='connection',
+                        default='test-connection',
+                        help='Connection name')
     parser.add_argument('--cluster-options', dest='cluster_options', nargs='+',
                         default=[], help='Cluster options using the format: '
                                          'key1=val1 [key2=val2...]')
@@ -60,10 +61,10 @@ if __name__ == "__main__":
     args, unknownargs = parser.parse_known_args()
     sys.argv = [sys.argv[0]] + unknownargs
 
-    ClusterTest.BACKEND = args.backend
-    ClusterTest.ENGINE = args.engine
-    ClusterTest.CLUSTER_CONFIG["name"] = args.cluster
-    ClusterTest.CLUSTER_CONFIG.update(
+    ConnectionTest.BACKEND = args.backend
+    ConnectionTest.ENGINE = args.engine
+    ConnectionTest.CLUSTER_CONFIG["name"] = args.connection
+    ConnectionTest.CLUSTER_CONFIG.update(
         dict(item.split('=') for item in args.cluster_options))
 
     unittest.main(verbosity=2)
