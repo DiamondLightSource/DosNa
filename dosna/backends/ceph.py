7653822883db71bfc9bf153f2c552cc083702099
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2017-11-06 16:05:00 +0000

    Use `Connection` in backend ceph

--- dosna/backends/ceph.py
+++ dosna/backends/ceph.py
@@ -1,75 +1,58 @@
+#!/usr/bin/env python
+"""
+Backend ceph uses a ceph cluster to store the dataset and chunks data
+"""
 
+import logging
 
-import logging as log
-
-import rados
 import numpy as np
+import rados
 
-from .. import Backend
-from ..base import BaseCluster, BasePool, BaseDataset, BaseDataChunk
-from ..utils import dtype2str, shape2str, str2shape
+from dosna import Backend
+from dosna.base import BaseConnection, BaseDataChunk, BaseDataset
+from dosna.utils import dtype2str, shape2str, str2shape
+
+_SIGNATURE = "DosNa Dataset"
+
+log = logging.getLogger(__name__)
 
 
-class CephCluster(BaseCluster):
+class CephConnection(BaseConnection):
     """
     A Ceph Cluster that wraps LibRados.Cluster
     """
 
     def __init__(self, name, conffile='ceph.conf', timeout=5,
-                 client_id=None, **kwargs):
-        super(CephCluster, self).__init__(name, **kwargs)
-        rados_options = {"conffile" : conffile}
+                 client_id=None, *args, **kwargs):
+        super(CephConnection, self).__init__(name, *args, **kwargs)
+
+        rados_options = {
+            "conffile": conffile
+        }
         if client_id is not None:
             client_name = "client.{}".format(client_id)
             rados_options["name"] = client_name
+
         self._cluster = rados.Rados(**rados_options)
         self._timeout = timeout
+        self._ioctx = None
 
     def connect(self):
+        if self.connected:
+            raise Exception('Connection {} is already open'.format(self.name))
         self._cluster.connect(timeout=self._timeout)
-        super(CephCluster, self).connect()
+        self._ioctx = self._cluster.open_ioctx(self.name)
+        super(CephConnection, self).connect()
 
     def disconnect(self):
-        self._cluster.shutdown()
-        super(CephCluster, self).disconnect()
+        if self.connected:
+            self.ioctx.close()
+            self._cluster.shutdown()
+            super(CephConnection, self).disconnect()
 
-    def create_pool(self, name, open_mode='a'):
-        if self.has_pool(name):
-            raise Exception('Pool `%s` already exists' % name)
-        self._cluster.create_pool(name)
-        return self.get_pool(name, open_mode=open_mode)
-
-    def has_pool(self, name):
-        return self._cluster.pool_exists(name)
-
-    def get_pool(self, name, open_mode='a'):
-        if self.has_pool(name):
-            return CephPool(self, name, open_mode=open_mode)
-        raise Exception('Pool `%s` does not exist' % name)
-
-    def del_pool(self, name):
-        if self.has_pool(name):
-            self._cluster.delete_pool(name)
-        else:
-            raise Exception('Pool `%s` does not exist' % name)
-
-
-class CephPool(BasePool):
-    """
-    A Ceph Pool wraps LibRados.Pool
-    """
-
-    __signature__ = "DosNa Dataset"
-
-    def open(self):
-        if self.isopen:
-            raise Exception('Pool {} is already open'.format(self.name))
-        self._ioctx = self._cluster._cluster.open_ioctx(self.name)
-        super(CephPool, self).open()
-
-    def close(self):
-        self._ioctx.close()
-        super(CephPool, self).close()
+    @property
+    def ioctx(self):
+        return self._ioctx
 
     def create_dataset(self, name, shape=None, dtype=np.float32, fillvalue=0,
                        data=None, chunks=None):
@@ -89,12 +72,12 @@ class CephPool(BasePool):
             csize = chunks
         chunks_needed = (np.ceil(np.asarray(shape, float) / csize)).astype(int)
 
-        self._ioctx.write(name, self.__signature__)
-        self._ioctx.set_xattr(name, 'shape', shape2str(shape))
-        self._ioctx.set_xattr(name, 'dtype', dtype2str(dtype))
-        self._ioctx.set_xattr(name, 'fillvalue', repr(fillvalue))
-        self._ioctx.set_xattr(name, 'chunks', shape2str(chunks_needed))
-        self._ioctx.set_xattr(name, 'chunk_size', shape2str(csize))
+        self.ioctx.write(name, _SIGNATURE)
+        self.ioctx.set_xattr(name, 'shape', shape2str(shape))
+        self.ioctx.set_xattr(name, 'dtype', dtype2str(dtype))
+        self.ioctx.set_xattr(name, 'fillvalue', repr(fillvalue))
+        self.ioctx.set_xattr(name, 'chunks', shape2str(chunks_needed))
+        self.ioctx.set_xattr(name, 'chunk_size', shape2str(csize))
 
         dataset = CephDataset(self, name, shape, dtype, fillvalue,
                               chunks_needed, csize)
@@ -104,19 +87,19 @@ class CephPool(BasePool):
     def get_dataset(self, name):
         if not self.has_dataset(name):
             raise Exception('Dataset `%s` does not exist' % name)
-        shape = str2shape(self._ioctx.get_xattr(name, 'shape'))
-        dtype = self._ioctx.get_xattr(name, 'dtype')
-        fillvalue = int(self._ioctx.get_xattr(name, 'fillvalue'))
-        chunks = str2shape(self._ioctx.get_xattr(name, 'chunks'))
-        chunk_size = str2shape(self._ioctx.get_xattr(name, 'chunk_size'))
+        shape = str2shape(self.ioctx.get_xattr(name, 'shape'))
+        dtype = self.ioctx.get_xattr(name, 'dtype')
+        fillvalue = int(self.ioctx.get_xattr(name, 'fillvalue'))
+        chunks = str2shape(self.ioctx.get_xattr(name, 'chunks'))
+        chunk_size = str2shape(self.ioctx.get_xattr(name, 'chunk_size'))
         dataset = CephDataset(self, name, shape, dtype, fillvalue,
                               chunks, chunk_size)
         return dataset
 
     def has_dataset(self, name):
         try:
-            valid = self._ioctx.stat(name)[0] == len(self.__signature__) and \
-                self._ioctx.read(name) == self.__signature__
+            valid = self.ioctx.stat(name)[0] == len(_SIGNATURE) and \
+                self.ioctx.read(name) == _SIGNATURE
         except rados.ObjectNotFound:
             return False
         return valid
@@ -124,9 +107,9 @@ class CephPool(BasePool):
     def del_dataset(self, name):
         if self.has_dataset(name):
             self.get_dataset(name).clear()
-            self._ioctx.remove_object(name)
+            self.ioctx.remove_object(name)
         else:
-            raise Exception('Dataset `%s` does not exist' % name)
+            raise Exception('Dataset `{}` does not exist'.format(name))
 
 
 class CephDataset(BaseDataset):
@@ -135,11 +118,11 @@ class CephDataset(BaseDataset):
     """
 
     @property
-    def _ioctx(self):
-        return self._pool._ioctx
+    def ioctx(self):
+        return self.connection.ioctx
 
     def _idx2name(self, idx):
-        return '%s/%s' % (self.name, '.'.join(map(str, idx)))
+        return '{}/{}'.format(self.name, '.'.join(map(str, idx)))
 
     def create_chunk(self, idx, data=None, slices=None):
         if self.has_chunk(idx):
@@ -151,7 +134,7 @@ class CephDataset(BaseDataset):
         cdata = np.full(shape, fillvalue, dtype)
         if data is not None:
             cdata[slices] = data
-        self._ioctx.write_full(name, cdata.tobytes())
+        self.ioctx.write_full(name, cdata.tobytes())
         return CephDataChunk(self, idx, name, shape, dtype, fillvalue)
 
     def get_chunk(self, idx):
@@ -166,14 +149,14 @@ class CephDataset(BaseDataset):
     def has_chunk(self, idx):
         name = self._idx2name(idx)
         try:
-            self._ioctx.stat(name)
+            self.ioctx.stat(name)
         except rados.ObjectNotFound:
             return False
         return True
 
     def del_chunk(self, idx):
         if self.has_chunk(idx):
-            self._ioctx.remove_object(self._idx2name(idx))
+            self.ioctx.remove_object(self._idx2name(idx))
 
     def clear(self):
         for idx in self._ndindex(self.chunks):
@@ -183,14 +166,14 @@ class CephDataset(BaseDataset):
 class CephDataChunk(BaseDataChunk):
 
     @property
-    def _ioctx(self):
-        return self._dataset._ioctx
+    def ioctx(self):
+        return self.dataset.ioctx
 
     def get_data(self, slices=None):
         if slices is None:
             slices = slice(None)
-        n = np.prod(self.shape)
-        data = np.fromstring(self.read(), dtype=self.dtype, count=n)
+        data_count = np.prod(self.shape)
+        data = np.fromstring(self.read(), dtype=self.dtype, count=data_count)
         data.shape = self.shape
         return data[slices]
 
@@ -202,13 +185,13 @@ class CephDataChunk(BaseDataChunk):
         self.write_full(cdata.tobytes())
 
     def write_full(self, data):
-        self._ioctx.write_full(self.name, data)
+        self.ioctx.write_full(self.name, data)
 
     def read(self, length=None, offset=0):
         if length is None:
             length = self.size * np.dtype(self.dtype).itemsize
-        return self._ioctx.read(self.name, length=length, offset=offset)
+        return self.ioctx.read(self.name, length=length, offset=offset)
 
 
-__backend__ = Backend('ceph', CephCluster, CephPool, CephDataset,
+__backend__ = Backend('ceph', CephConnection, CephDataset,
                       CephDataChunk)
