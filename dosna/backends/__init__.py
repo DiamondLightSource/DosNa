f42970732146246e5c45d74cf03b7fa23da874a0
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2017-11-06 16:06:14 +0000

    Improve code style in backends/__init__.py

--- dosna/backends/__init__.py
+++ dosna/backends/__init__.py
@@ -1,24 +1,28 @@
-
+#!/usr/bin/env python
+"""
+Helper functions to save the selected backend
+"""
 
 import logging
 from importlib import import_module
 
 log = logging.getLogger(__name__)
 
-__current = None
+_current = None
 available = ['ram', 'hdf5', 'ceph']
 
 
 def use_backend(backend):
     backend = backend.lower()
-    global __current
+    global _current
     if backend in available:
-        m = import_module('dosna.backends.%s' % backend)
-        if hasattr(m, '__backend__'):
-            log.debug('Switching backend to `%s`' % m.__backend__.name)
-            __current = m.__backend__
+        module = import_module('dosna.backends.{}'.format(backend))
+        if hasattr(module, '__backend__'):
+            log.debug('Switching backend to `%s`', module.__backend__.name)
+            _current = module.__backend__
         else:
-            raise Exception('Module `%s` is not a proper backend.' % backend)
+            raise Exception(
+                'Module `{}` is not a proper backend.'.format(backend))
     else:
         raise Exception('Backend `{}` not available! Choose from: {}'
                         .format(backend, available))
@@ -27,6 +31,6 @@ def use_backend(backend):
 def get_backend(name=None):
     if name is not None:
         use_backend(name)
-    if __current is None:
+    if _current is None:
         use_backend(available[0])
-    return __current
+    return _current
