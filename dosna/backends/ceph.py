67726634e5d91429620cca882e766c7be5f05f0e
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2018-07-20 09:41:12 +0100

    Ceph: Solve non-exact created chunk issue

--- dosna/backends/ceph.py
+++ dosna/backends/ceph.py
@@ -140,7 +140,7 @@ class CephDataset(BackendDataset):
         datachunk = CephDataChunk(self, idx, name, shape, dtype, fillvalue)
         if data is None:
             data = np.full(shape, fillvalue, dtype)
-        datachunk.set_data(data, slices)
+        datachunk.set_data(data, slices, fill_others=True)
         return datachunk
 
     def get_chunk(self, idx):
@@ -178,11 +178,14 @@ class CephDataChunk(BackendDataChunk):
         data.shape = self.shape
         return data[slices]
 
-    def set_data(self, values, slices=None):
+    def set_data(self, values, slices=None, fill_others=False):
         if slices is None or slices2shape(slices) == self.shape:
             self.write_full(values.tobytes())
         else:
-            cdata = self.get_data()
+            if fill_others:
+                cdata = np.full(self.shape, self.fillvalue, self.dtype)
+            else:
+                cdata = self.get_data()
             cdata[slices] = values
             self.write_full(cdata.tobytes())
 
