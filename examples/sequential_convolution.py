5fb18fce3b17ec730e82904cb575cfc4e7d766c2
Emilio Perez <emilio.perez-juarez@diamond.ac.uk>
2018-08-10 15:32:18 +0100

    sequential_convolution: add time for deleting and refactor

--- examples/sequential_convolution.py
+++ examples/sequential_convolution.py
@@ -77,19 +77,18 @@ def convolve1(ds, sigma, out_path):
     ds3d_ = ds.clone(DS1_NAME)
     timer = Timer('Separable 3D convolution')
 
-    timer.__enter__()
-    for z in range(ds.chunk_grid[0]):
-        zS = z * ds.chunk_size[0]
-        zE = min(zS + ds.chunk_size[0], ds.shape[0])
-        for y in range(ds.chunk_grid[1]):
-            yS = y * ds.chunk_size[1]
-            yE = min(yS + ds.chunk_size[1], ds.shape[1])
-            for x in range(ds.chunk_grid[2]):
-                xS = x * ds.chunk_size[2]
-                xE = min(xS + ds.chunk_size[2], ds.shape[2])
-                out = gaussian_filter(ds[zS:zE, yS:yE, xS:xE], sigma=sigma)
-                ds3d_[zS:zE, yS:yE, xS:xE] = out
-    timer.__exit__()
+    with timer:
+        for z in range(ds.chunk_grid[0]):
+            zS = z * ds.chunk_size[0]
+            zE = min(zS + ds.chunk_size[0], ds.shape[0])
+            for y in range(ds.chunk_grid[1]):
+                yS = y * ds.chunk_size[1]
+                yE = min(yS + ds.chunk_size[1], ds.shape[1])
+                for x in range(ds.chunk_grid[2]):
+                    xS = x * ds.chunk_size[2]
+                    xE = min(xS + ds.chunk_size[2], ds.shape[2])
+                    out = gaussian_filter(ds[zS:zE, yS:yE, xS:xE], sigma=sigma)
+                    ds3d_[zS:zE, yS:yE, xS:xE] = out
 
     try:
         imwrite(join(out_path,
@@ -111,42 +110,40 @@ def convolve2(ds, sigma, out_path):
 
     timer = Timer('Three separable 1D convolutions')
 
-    timer.__enter__()
-    print('Convolving axis Z')
+    with timer:
+        print('Convolving axis Z')
 
-    ds1_ = ds.clone(DS2Z_NAME)
+        ds1_ = ds.clone(DS2Z_NAME)
 
-    for z in range(ds.chunk_grid[0]):
-        zS = z * ds.chunk_size[0]
-        zE = min(zS + ds.chunk_size[0], ds.shape[0])
-        out = gaussian_filter1d(ds[zS:zE], sigma=sigma, axis=0)
-        ds1_[zS:zE] = out
+        for z in range(ds.chunk_grid[0]):
+            zS = z * ds.chunk_size[0]
+            zE = min(zS + ds.chunk_size[0], ds.shape[0])
+            out = gaussian_filter1d(ds[zS:zE], sigma=sigma, axis=0)
+            ds1_[zS:zE] = out
 
-    # Gaussian second axis
+        # Gaussian second axis
 
-    print('Convolving axis Y')
+        print('Convolving axis Y')
 
-    ds2_ = ds.clone(DS2Y_NAME)
+        ds2_ = ds.clone(DS2Y_NAME)
 
-    for y in range(ds.chunk_grid[1]):
-        yS = y * ds.chunk_size[1]
-        yE = min(yS + ds.chunk_size[1], ds.shape[1])
-        out = gaussian_filter1d(ds1_[:, yS:yE], sigma=sigma, axis=1)
-        ds2_[:, yS:yE] = out
+        for y in range(ds.chunk_grid[1]):
+            yS = y * ds.chunk_size[1]
+            yE = min(yS + ds.chunk_size[1], ds.shape[1])
+            out = gaussian_filter1d(ds1_[:, yS:yE], sigma=sigma, axis=1)
+            ds2_[:, yS:yE] = out
 
-    # Gaussian second axis
+        # Gaussian second axis
 
-    print('Convolving axis X')
+        print('Convolving axis X')
 
-    ds3_ = ds.clone(DS2X_NAME)
+        ds3_ = ds.clone(DS2X_NAME)
 
-    for x in range(ds.chunk_grid[2]):
-        xS = x * ds.chunk_size[2]
-        xE = min(xS + ds.chunk_size[2], ds.shape[2])
-        out = gaussian_filter1d(ds2_[..., xS:xE], sigma=sigma, axis=2)
-        ds3_[..., xS:xE] = out
-
-    timer.__exit__()
+        for x in range(ds.chunk_grid[2]):
+            xS = x * ds.chunk_size[2]
+            xE = min(xS + ds.chunk_size[2], ds.shape[2])
+            out = gaussian_filter1d(ds2_[..., xS:xE], sigma=sigma, axis=2)
+            ds3_[..., xS:xE] = out
 
     try:
         imwrite(join(out_path, '{}_conv3x1d_{}-{}.png'.format(PREFIX,
@@ -197,10 +194,9 @@ def main():
             with dn.Connection(**connection_config) as connection:
                 print('Loading Data -- shape: {} chunk_size: {}'.format(DS, CS))
                 t = Timer('Data loaded')
-                t.__enter__()
-                dataset = connection.create_dataset(IN_DS_NAME, data=data,
-                                                    chunk_size=(CS, CS, CS))
-                t.__exit__()
+                with t:
+                    dataset = connection.create_dataset(IN_DS_NAME, data=data,
+                                                        chunk_size=(CS, CS, CS))
 
                 result_info.append({
                     'create_time': t.time,
@@ -217,8 +213,10 @@ def main():
                     result_info[-1]['conv1_times'].append(t1)
                     t2 = convolve2(dataset, sigma, out_path)
                     result_info[-1]['conv2_times'].append(t2)
-
-                dataset.delete()
+                t = Timer('Data deleted')
+                with t:
+                    dataset.delete()
+                result_info[-1]['delete_time'] = t.time
 
     json.dump(result_info, open(join(out_path, RESULT), "w"),
               sort_keys=True, indent=4, separators=(',', ': '))
