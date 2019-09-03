433b4663afa7010c671f6e9408c608262c6270f8
Gabryel Mason-Williams crc99971 <gabryel.mason-williams@diamond.ac.uk>
2019-09-03 09:35:50 +0100

    Argument Parser to be used to connect to ceph

--- dosna/webapp/webapp.py
+++ dosna/webapp/webapp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """ Web app for dosna over ceph to see and display objects"""
 from flask import Flask, render_template
-
+import argparse
 import numpy as np
 
 import rados
@@ -9,8 +9,15 @@ import dosna as dn
 
 from PIL import Image  
 
-app = Flask(__name__)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Webapp')
+    parser.add_argument('--connection-options', dest='connection_options',
+                        nargs='+', default=[],
+                        help='Cluster options using the format: '
+                             'key1=val1 [key2=val2...]')
+    return parser.parse_args()
 
+app = Flask(__name__)
 # Home page
 @app.route('/')
 def index():
@@ -19,6 +26,7 @@ def index():
 # Listing objects inside pool
 @app.route('/list/obj/<pool>')
 def list_object(pool):
+
     return render_template('listObjects.html')
 
 # Display object as a string
@@ -32,4 +40,7 @@ def display_image_object(pool,obj):
     return render_template('objectImage.html',obj=obj)
 
 if __name__ == '__main__':
+    args = parse_args()
+	connection_config = {}
+	connection_config.update(dict(item.split('=') for item in args.connection_options))
 	app.run(debug=True, host='0.0.0.0')
