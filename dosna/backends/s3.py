ae46f264fa6b3abede193140c1d56770c057947f
Paula Martin <paula.martin-gomez@diamond.ac.uk>
2021-06-18 14:18:36 +0100

    add incomplete s3

--- dosna/backends/s3.py
+++ dosna/backends/s3.py
@@ -1,9 +1,10 @@
 
 import logging
 
+import time
 import boto3
 import numpy as np
-
+import time
 from botocore.exceptions import ClientError
 from dosna.backends import Backend
 from dosna.backends.base import (BackendConnection, BackendDataChunk,
@@ -45,14 +46,15 @@ class S3Connection(BackendConnection):
         self._aws_access_key = aws_access_key
         self._aws_secret_access_key = aws_secret_access_key
 
-        #self._root_group = S3Group(self,_ROOT_GROUP,attrs={})
-        self.groups = {}
-        self.datasets = {}
-
+        self.datasets = {} # TODO
 
         super(S3Connection, self).__init__(name, *args, **kwargs)
 
     def connect(self):
+        """
+        Creates a service client and checks whether a bucket exists
+        and have permission to access it.
+        """
 
         if self.connected:
             raise ConnectionError(
@@ -90,6 +92,53 @@ class S3Connection(BackendConnection):
     def client(self):
         return self._client
 
+    # Group methods
+    def create_group(self, name, attrs={}):
+
+        absolute_path = self.name + _PATH_SPLIT + name
+        attrs = {str(key): str(value) for key, value in attrs.items()}
+        response = self.client.put_object(Bucket=self.bucket, Key=absolute_path,
+                               Body=str.encode('Group'), Metadata=attrs)
+        group = S3Group(self, absolute_path, attrs)
+        return group
+
+    def get_group(self, name): # TODO
+
+        try:
+            response = self.client.get_object(Bucket=self.bucket, Key=name)['Body'].read()
+            return response
+        except Exception as e:
+            print('Group: ', name, 'does not exist. ', e)
+
+
+    def has_group(self, name):
+        try:
+            self.client.head_object(Bucket=self.bucket, Key=name)
+            return True
+        except Exception as e:
+            print('Group not found: ', name, 'Error: ',  e)
+            return False
+
+    def del_group(self, name):
+
+        paginator = self.client.get_paginator('list_objects_v2')
+        operation_parameters = {'Bucket': self.bucket,
+                                    'Prefix': name}
+        page_iterator = paginator.paginate(**operation_parameters)
+        for page in page_iterator:
+            key_count = page['KeyCount']
+            if key_count > 0:
+                for obj in page['Contents']:
+                    response = self.client.delete_object(Bucket=self.bucket, Key=obj['Key'])
+                    if response is not None:
+                        print('Deleting', obj)
+                    else:
+                        print('Could not delete object', obj)
+        else:
+            raise Exception('Could not found objects with prefix: ', name)
+
+
+    # Dataset methods
     def create_dataset(self, name, shape=None, dtype=np.float32, fillvalue=0,
                        data=None, chunk_size=None):
 
@@ -135,54 +184,82 @@ class S3Connection(BackendConnection):
 
         return dataset
 
-    def create_group(self, name, attrs={}):
-
-        keys_values = attrs.items()
-
-        attrs = {str(key): str(value) for key, value in keys_values}
-
-        absolute_path = self.name + _PATH_SPLIT + name
-
-        self.client.put_object(Bucket=self.bucket, Key=absolute_path,
-                               Body=str.encode('Group')) #Metadata=attrs)
-
-        group = S3Group(self, absolute_path, attrs)
-
-        self.groups[absolute_path] = group
-
-        return group
-
-    def get_group(self, name):
+    def get_dataset(self, name): # TODO
 
         try:
             response = self.client.get_object(Bucket=self.bucket, Key=name)['Body'].read()
-            print(response)
-        except Exception as e:
-            print('Group: ', name, 'does not exist. ', e)
-
-        print('selfgroups', self.groups)
-        return response
-
-    def get_dataset(self, name):
-
-        try:
-            response = self.client.get_object(Bucket=self.bucket, Key=name)['Body'].read()
-            print(response)
         except Exception as e:
             print('Group: ', name, 'does not exist. ', e)
 
         return self.datasets[name]
 
+    def has_dataset(self, name):
+        pass
+
+    def del_dataset(self, name):
+        pass
+
+    def get_object_metadata(self, name):
+        """
+        Return an object's metadata.
+        """
+        metadata = self.client.head_object(Bucket=self.bucket, Key=name)['Metadata']
+        return metadata
+
+    # Object methods
+
+    def get_object(self):
+        try:
+            response = self.client.get_object(Bucket=self.bucket, Key=name)['Body'].read()
+            return response
+        except Exception as e:
+            print('Group: ', name, 'does not exist. ', e)
+
+    def visit_groups(self):
+        """
+        Returns a list of objects with keys that begin with this connection name.
+        """
+        paginator = self.client.get_paginator('list_objects_v2')
+        operation_parameters = {'Bucket': self.bucket,
+                                'Prefix': self.name}
+        page_iterator = paginator.paginate(**operation_parameters)
+        groups_list = []
+        for page in page_iterator:
+            print("Another page")
+
+            if page['KeyCount'] > 0:
+                for obj in page['Contents']:
+                    metadata = self.client.head_object(Bucket=self.bucket, Key=obj['Key'])['Metadata']
+                    s3type = metadata.get('s3type', None)
+                    if s3type == 'Group':
+                        groups_list.append(obj['Key'])
+        return groups_list
+
+
     def visit_objects(self):
+        """
+        Returns a list of objects with keys that begin with this connection name.
+        """
+        paginator = self.client.get_paginator('list_objects_v2')
+        operation_parameters = {'Bucket': self.bucket,
+                                'Prefix': self.name}
+        page_iterator = paginator.paginate(**operation_parameters)
+        for page in page_iterator:
+            key_count = page['KeyCount']
+        if key_count > 0:
+            objects_list = [obj['Key'] for page in page_iterator for obj in page['Contents']]
+            return objects_list
+        else:
+            raise Exception('No objects found with prefix: ', self.name)
 
-        prefix = self.name
+    def get_metadata(self, name):
+        """
+        Return an object's metadata.
+        """
+        metadata = self.client.head_object(Bucket=self.bucket, Key=name)['Metadata']
+        return metadata
 
-        files = self.client.list_objects_v2(Bucket=self.bucket, Prefix=prefix)
-        objects_list = []
-        for f in files['Contents']:
-            objects_list.append(f['Key'])
 
-        return objects_list
 
 
 class S3Group(BackendGroup):
@@ -195,22 +272,17 @@ class S3Group(BackendGroup):
     def bucket(self):
         return self.parent.bucket
 
+    # Group methods
 
     def create_group(self, name, attrs={}):
 
-        keys_values = attrs.items()
-
-        attrs = {str(key): str(value) for key, value in keys_values}
-
         absolute_path = self.name + _PATH_SPLIT + name
-
-        self.client.put_object(Bucket=self.bucket, Key=absolute_path,
-                               Body=str.encode('Group')) #Metadata=attrs)
+        attrs = {str(key): str(value) for key, value in attrs.items()}
+        response = self.client.put_object(Bucket=self.bucket, Key=absolute_path,
+                               Body=str.encode('Group'), Metadata=attrs)
 
         group = S3Group(self, absolute_path, attrs)
-
         return group
-
 
     def get_group(self, name):
 
@@ -219,8 +291,7 @@ class S3Group(BackendGroup):
         except Exception as e:
             print('Group: ', name, 'does not exist. ', e)
 
-        return response # TODO return S3GROUP
-
+        return response
 
     def has_group(self, name):
         try:
@@ -245,13 +316,13 @@ class S3Group(BackendGroup):
                 else:
                     print('Could not delete object', f[key])
 
+    # Dataset methods
 
     def create_dataset(self, name, shape=None, dtype=np.float32, fillvalue=0,
                        data=None, chunk_size=None):
 
         if not ((shape is not None and dtype is not None) or data is not None):
             raise Exception('Provide `shape` and `dtype` or `data`')
-        # TODO restore this but with the appropiate function
 #        if self.has_dataset(name):
 #            raise Exception('Dataset `%s` already exists' % name)
 
@@ -278,7 +349,6 @@ class S3Group(BackendGroup):
         self.client.put_object(Bucket=self.bucket,Key=absolute_path,
                                Body=str.encode(_SIGNATURE),Metadata=metadata)
 
-        # TODO NAME of dataset
         dataset = S3Dataset(self, absolute_path, shape, dtype,fillvalue, chunk_grid, chunk_size)
 
         return dataset
@@ -290,7 +360,7 @@ class S3Group(BackendGroup):
         except Exception as e:
             print('Group: ', name, 'does not exist. ', e)
 
-        return response  # TODO return S3GROUP
+        return response
 
     def has_dataset(self, name):
         try:
@@ -300,6 +370,59 @@ class S3Group(BackendGroup):
 
     def del_dataset(self, name):
         pass
+
+
+    def visit_groups(self): # TODO
+        """
+        Returns a list of objects with keys that begin with this connection name.
+        """
+        paginator = self.client.get_paginator('list_objects_v2')
+        operation_parameters = {'Bucket': self.bucket,
+                                'Prefix': self.name}
+        page_iterator = paginator.paginate(**operation_parameters)
+        for page in page_iterator:
+            print(page)
+            #key_count = page['KeyCount']
+        """
+        if key_count > 0:
+            objects_list = [obj['Key'] for page in page_iterator for obj in page['Contents']]
+            return objects_list
+        else:
+            raise Exception('No objects found with prefix: ', self.name)
+        """
+
+    # Object methods
+    def visit_objects(self):
+        """
+        Returns a list of objects with keys that begin with this connection name.
+        """
+        paginator = self.client.get_paginator('list_objects_v2')
+        operation_parameters = {'Bucket': self.bucket,
+                                'Prefix': self.name}
+        page_iterator = paginator.paginate(**operation_parameters)
+        for page in page_iterator:
+            key_count = page['KeyCount']
+        if key_count > 0:
+            objects_list = [obj['Key'] for page in page_iterator for obj in page['Contents']]
+            return objects_list
+        else:
+            raise Exception('No objects found with prefix: ', self.name)
+
+
+
+    def get_object_metadata(self, name):
+        """
+        Return an object's metadata.
+        """
+        metadata = self.client.head_object(Bucket=self.bucket, Key=name)['Metadata']
+        return metadata
+
+    def add_connection(self, metadata): # TODO
+        metadata = {str(key): str(value) for key, value in metadata.items()}
+        metadata = self.client.put_object(Bucket=self.bucket,
+                                          Key=self.name,
+                                          Body=str.encode('Connection'),
+                                          Metadata=metadata)
 
 
 class S3Dataset(BackendDataset):
@@ -396,9 +519,8 @@ class S3DataChunk(BackendDataChunk):
 
     def write_full(self, data):
         absolute_path = self.dataset.name + "/" + self.name
-        print('Writing data_chunk')
         self.client.put_object(
-            Bucket=self.dataset.bucket, Key=absolute_path, Body=data, # TODO check if tagging works
+            Bucket=self.dataset.bucket, Key=absolute_path, Body=data
         )
 
     def read(self, length=None, offset=0):
